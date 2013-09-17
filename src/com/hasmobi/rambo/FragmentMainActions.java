249f0b085bcd28d8bc456371bfe724d5bbe4cdeb
hasmobi <hasmobi@1849bb56-f0e2-48ca-bffa-162ff6fef5c1>
2013-09-17 17:26:50 +0000

    Included some basic FlowLayout for Main Actions fragment
    git-svn-id: https://hasmoni.unfuddle.com/svn/hasmoni_rambo/trunk@40 1849bb56-f0e2-48ca-bffa-162ff6fef5c1

--- src/com/hasmobi/rambo/FragmentMainActions.java
+++ src/com/hasmobi/rambo/FragmentMainActions.java
@@ -1,23 +1,54 @@
 package com.hasmobi.rambo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class FragmentMainActions extends DFragment {
+public class FragmentMainActions extends DFragment implements OnClickListener {
+
+	Context c;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
+		c = getActivity().getBaseContext();
+
 		super.onCreate(savedInstanceState);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.xml.fragment_main_actions, null);
+		View layout = inflater.inflate(R.xml.fragment_main_actions, null);
+
+		// Open Pie icon
+//		LinearLayout i1 = (LinearLayout) layout.findViewById(R.id.llOpenPie);
+//		// Running Apps icon
+//		LinearLayout i2 = (LinearLayout) layout
+//				.findViewById(R.id.llOpenRunningApps);
+//
+//		i1.setOnClickListener(this);
+//		i2.setOnClickListener(this);
+
+		return layout;
+	}
+
+	public void onClick(View v) {
+//		switch (v.getId()) {
+//		case R.id.llOpenPie:
+//			Intent i = new Intent(c, PieActivity.class);
+//			i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//			startActivity(i);
+//			break;
+//		}
 	}
 
 }
--- src/com/hasmobi/rambo/StartActivity.java
+++ src/com/hasmobi/rambo/StartActivity.java
@@ -6,8 +6,6 @@ import com.hasmobi.rambo.utils.FeedbackManager;
 import com.hasmobi.rambo.utils.Prefs;
 import com.hasmobi.rambo.utils.Values;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -17,15 +15,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources.NotFoundException;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class StartActivity extends DActivity {
